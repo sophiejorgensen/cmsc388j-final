e51e4e6b9be90d8d7b2f1a5c626549d181cc6f90
Sophie Jorgensen <sophjorgy25@gmail.com>
2021-12-10 12:22:36 -0500

    mess

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,3 +1,3 @@
 # Stores all configuration values
 SECRET_KEY = b',f\xfe~\x08}\xf8Z5B\x9dX6\xc6\xb8\x9f'
-MONGODB_HOST = "mongodb://localhost:27017/my_database"
+#MONGODB_HOST = "mongodb://localhost:27017/my_database"
